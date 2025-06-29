c66ff702b32ae09af150f27269b588254464f301
Alexander Zeitlhofer <alexander.zeitlhofer@sz-ybbs.ac.at>
2025-06-29 10:28:30 +0200

    added primitive backend for projecxtrs, no image supp yet

--- app/Projects.tsx
+++ app/Projects.tsx
@@ -1,8 +1,7 @@
 import { useRef } from "react";
 import { Figtree } from "next/font/google";
-// import { list } from "@vercel/blob";
 import { useState } from "react";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 
 const figtree = Figtree({
   weight: "700",
@@ -13,6 +12,7 @@ type Project = {
   title: string;
   image: string;
   description: string;
+  createdAt: Date;
 };
 
 // type BlobMeta = {
@@ -23,11 +23,11 @@ type Project = {
 //   contentType: string;
 // };
 
-// Example card data – replace with your own
+// Example card data
 // const projects: Project[] = [
 //   {
 //     title: "Griessler Website",
-//     image: "./fave_edited.png",
+//     image: "./images/griessler.png",
 //     description:
 //       "Im Rahmen eines Schulprojekts wurde die Griessler Website neu designed.",
 //   },
@@ -36,8 +36,31 @@ type Project = {
 export default function Projects() {
   const scrollRef = useRef<HTMLDivElement>(null);
 
-  // setProjects missing
-  const [projects] = useState<Project[]>([]);
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const res = await fetch("/api/projects", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
+          },
+        });
+
+        if (!res.ok) throw new Error(`Error: ${res.status}`);
+
+        const projects = await res.json();
+
+        setProjects(projects);
+      } catch (error) {
+        console.error(`Error fetching Projects: ${error}`);
+      }
+    };
+
+    fetchProjects();
+  }, []);
 
   return (
     <div
@@ -123,30 +146,39 @@ export default function Projects() {
             msOverflowStyle: "none", // IE 10+
           }}
         >
-          {projects.map((project, idx) => (
-            <div
-              key={idx}
-              className="flex-shrink-0 max-w-screen lg:max-w-[33.3vw] bg-foreground shadow-lg flex flex-col items-center relative group"
-            >
-              <div className="relative w-full hover:z-30 h-[70vh] md:h-[30vh] lg:h-[70vh]">
-                <div className="w-full h-full transition-transform duration-300 transform group-hover:scale-105">
-                  <img
-                    src={project.image}
-                    alt={project.title}
-                    className="w-full h-full grayscale group-hover:grayscale-0 transition-all duration-300 object-cover"
-                  />
-                  <div className="absolute bottom-0 left-0 w-full bg-black/60 px-6 py-4 flex flex-col items-start">
-                    <h2 className="text-2xl font-bold text-white mb-1">
-                      {project.title}
-                    </h2>
-                    <p className="text-gray-200 text-sm">
-                      {project.description}
-                    </p>
+          {projects ? (
+            projects.map((project, idx) => (
+              <div
+                key={idx}
+                className="flex-shrink-0 max-w-screen lg:max-w-[33.3vw] bg-foreground shadow-lg flex flex-col items-center relative group"
+              >
+                <div className="relative w-full hover:z-30 h-[70vh] md:h-[30vh] lg:h-[70vh]">
+                  <div className="w-full h-full transition-transform duration-300 transform group-hover:scale-105">
+                    <img
+                      src={
+                        project.image
+                          ? project.image
+                          : "./images/No_image_available.png"
+                      }
+                      alt={project?.title}
+                      className="w-full h-full grayscale group-hover:grayscale-0 transition-all duration-300 object-cover"
+                    />
+
+                    <div className="absolute bottom-0 left-0 w-full bg-black/60 px-6 py-4 flex flex-col items-start">
+                      <h2 className="text-2xl font-bold text-white mb-1">
+                        {project?.title}
+                      </h2>
+                      <p className="text-gray-200 text-sm">
+                        {project?.description}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            <h2>Loading</h2>
+          )}
         </div>
         {/* Right Scroll Button */}
         <button
---
+++ app/generated/prisma/index.js
@@ -0,0 +1,200 @@
+
+Object.defineProperty(exports, "__esModule", { value: true });
+
+const {
+  PrismaClientKnownRequestError,
+  PrismaClientUnknownRequestError,
+  PrismaClientRustPanicError,
+  PrismaClientInitializationError,
+  PrismaClientValidationError,
+  getPrismaClient,
+  sqltag,
+  empty,
+  join,
+  raw,
+  skip,
+  Decimal,
+  Debug,
+  objectEnumValues,
+  makeStrictEnum,
+  Extensions,
+  warnOnce,
+  defineDmmfProperty,
+  Public,
+  getRuntime,
+  createParam,
+} = require('./runtime/library.js')
+
+
+const Prisma = {}
+
+exports.Prisma = Prisma
+exports.$Enums = {}
+
+/**
+ * Prisma Client JS version: 6.6.0
+ * Query Engine version: f676762280b54cd07c770017ed3711ddde35f37a
+ */
+Prisma.prismaVersion = {
+  client: "6.6.0",
+  engine: "f676762280b54cd07c770017ed3711ddde35f37a"
+}
+
+Prisma.PrismaClientKnownRequestError = PrismaClientKnownRequestError;
+Prisma.PrismaClientUnknownRequestError = PrismaClientUnknownRequestError
+Prisma.PrismaClientRustPanicError = PrismaClientRustPanicError
+Prisma.PrismaClientInitializationError = PrismaClientInitializationError
+Prisma.PrismaClientValidationError = PrismaClientValidationError
+Prisma.Decimal = Decimal
+
+/**
+ * Re-export of sql-template-tag
+ */
+Prisma.sql = sqltag
+Prisma.empty = empty
+Prisma.join = join
+Prisma.raw = raw
+Prisma.validator = Public.validator
+
+/**
+* Extensions
+*/
+Prisma.getExtensionContext = Extensions.getExtensionContext
+Prisma.defineExtension = Extensions.defineExtension
+
+/**
+ * Shorthand utilities for JSON filtering
+ */
+Prisma.DbNull = objectEnumValues.instances.DbNull
+Prisma.JsonNull = objectEnumValues.instances.JsonNull
+Prisma.AnyNull = objectEnumValues.instances.AnyNull
+
+Prisma.NullTypes = {
+  DbNull: objectEnumValues.classes.DbNull,
+  JsonNull: objectEnumValues.classes.JsonNull,
+  AnyNull: objectEnumValues.classes.AnyNull
+}
+
+
+
+
+  const path = require('path')
+
+/**
+ * Enums
+ */
+exports.Prisma.TransactionIsolationLevel = makeStrictEnum({
+  ReadUncommitted: 'ReadUncommitted',
+  ReadCommitted: 'ReadCommitted',
+  RepeatableRead: 'RepeatableRead',
+  Serializable: 'Serializable'
+});
+
+exports.Prisma.ProjectScalarFieldEnum = {
+  id: 'id',
+  title: 'title',
+  image: 'image',
+  description: 'description',
+  createdAt: 'createdAt'
+};
+
+exports.Prisma.SortOrder = {
+  asc: 'asc',
+  desc: 'desc'
+};
+
+exports.Prisma.QueryMode = {
+  default: 'default',
+  insensitive: 'insensitive'
+};
+
+
+exports.Prisma.ModelName = {
+  Project: 'Project'
+};
+/**
+ * Create the Client
+ */
+const config = {
+  "generator": {
+    "name": "client",
+    "provider": {
+      "fromEnvVar": null,
+      "value": "prisma-client-js"
+    },
+    "output": {
+      "value": "/Users/alexzeitlhofer/Documents/CodingProjects/homepage2025/app/generated/prisma",
+      "fromEnvVar": null
+    },
+    "config": {
+      "engineType": "library"
+    },
+    "binaryTargets": [
+      {
+        "fromEnvVar": null,
+        "value": "darwin-arm64",
+        "native": true
+      }
+    ],
+    "previewFeatures": [],
+    "sourceFilePath": "/Users/alexzeitlhofer/Documents/CodingProjects/homepage2025/prisma/schema.prisma",
+    "isCustomOutput": true
+  },
+  "relativeEnvPaths": {
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../../.env"
+  },
+  "relativePath": "../../../prisma",
+  "clientVersion": "6.6.0",
+  "engineVersion": "f676762280b54cd07c770017ed3711ddde35f37a",
+  "datasourceNames": [
+    "db"
+  ],
+  "activeProvider": "postgresql",
+  "inlineDatasources": {
+    "db": {
+      "url": {
+        "fromEnvVar": "PRISMA_DATABASE_URL",
+        "value": "prisma+postgres://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiMDFKWVdBTkhDU0syWTZDUkVFSlc3OTBTM1AiLCJ0ZW5hbnRfaWQiOiIyOTRlODU2MjU3NTczNTBkMjljZTcwNDAxZjQ5NThhNTgzZjhhYTBlZjJjZDI5NmFkNWU2NGExN2E5YjVjNjUwIiwiaW50ZXJuYWxfc2VjcmV0IjoiYTM4ODBlMTAtMDgzZS00YmE2LTljYjAtMDBjYmI5NzJhOTMwIn0.KtA6sMZH7QOOoKOGY42BjlJ2kB6nbVBQf_SCNU14k4c"
+      }
+    }
+  },
+  "inlineSchema": "// This is your Prisma schema file,\n// learn more about it in the docs: https://pris.ly/d/prisma-schema\n\n// Looking for ways to speed up your queries, or scale easily with your serverless or edge functions?\n// Try Prisma Accelerate: https://pris.ly/cli/accelerate-init\n\ngenerator client {\n  provider = \"prisma-client-js\"\n  output   = \"../app/generated/prisma\"\n}\n\ndatasource db {\n  provider = \"postgresql\"\n  url      = env(\"PRISMA_DATABASE_URL\")\n}\n\nmodel Project {\n  id          Int      @id @default(autoincrement())\n  title       String\n  image       String\n  description String\n  createdAt   DateTime @default(now())\n}\n",
+  "inlineSchemaHash": "fcd3e37dc8870d662de56fcddbd909dfb2e0f747c45ee29a5eaf1b0328ab32b5",
+  "copyEngine": false
+}
+
+const fs = require('fs')
+
+config.dirname = __dirname
+if (!fs.existsSync(path.join(__dirname, 'schema.prisma'))) {
+  const alternativePaths = [
+    "app/generated/prisma",
+    "generated/prisma",
+  ]
+  
+  const alternativePath = alternativePaths.find((altPath) => {
+    return fs.existsSync(path.join(process.cwd(), altPath, 'schema.prisma'))
+  }) ?? alternativePaths[0]
+
+  config.dirname = path.join(process.cwd(), alternativePath)
+  config.isBundled = true
+}
+
+config.runtimeDataModel = JSON.parse("{\"models\":{\"Project\":{\"dbName\":null,\"schema\":null,\"fields\":[{\"name\":\"id\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":true,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"Int\",\"nativeType\":null,\"default\":{\"name\":\"autoincrement\",\"args\":[]},\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"title\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"image\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"description\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":false,\"type\":\"String\",\"nativeType\":null,\"isGenerated\":false,\"isUpdatedAt\":false},{\"name\":\"createdAt\",\"kind\":\"scalar\",\"isList\":false,\"isRequired\":true,\"isUnique\":false,\"isId\":false,\"isReadOnly\":false,\"hasDefaultValue\":true,\"type\":\"DateTime\",\"nativeType\":null,\"default\":{\"name\":\"now\",\"args\":[]},\"isGenerated\":false,\"isUpdatedAt\":false}],\"primaryKey\":null,\"uniqueFields\":[],\"uniqueIndexes\":[],\"isGenerated\":false}},\"enums\":{},\"types\":{}}")
+defineDmmfProperty(exports.Prisma, config.runtimeDataModel)
+config.engineWasm = undefined
+config.compilerWasm = undefined
+
+
+const { warnEnvConflicts } = require('./runtime/library.js')
+
+warnEnvConflicts({
+    rootEnvPath: config.relativeEnvPaths.rootEnvPath && path.resolve(config.dirname, config.relativeEnvPaths.rootEnvPath),
+    schemaEnvPath: config.relativeEnvPaths.schemaEnvPath && path.resolve(config.dirname, config.relativeEnvPaths.schemaEnvPath)
+})
+
+const PrismaClient = getPrismaClient(config)
+exports.PrismaClient = PrismaClient
+Object.assign(exports, Prisma)
+
