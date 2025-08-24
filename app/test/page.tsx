a4d7386262491a3b090ebb9002a94db412fd70a3
Alexander Zeitlhofer <alexander.zeitlhofer@sz-ybbs.ac.at>
2025-08-24 11:36:04 +0200

    fck eslint

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,5 +1,4 @@
 import SectionTemplate from "@/components/SectionTemplate";
-import TextScramble from "@/components/TextScramble";
 export default function Page() {
   return (
     <SectionTemplate title="PROJEKTE" titlePosition="left"></SectionTemplate>
